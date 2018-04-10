21fbb60ee6923e037df4906d796195e1f68b4017
Toomas Vahter <toomas.vahter@gmail.com>
2018-04-10 11:04:15 +0300

    Disable navigation bar in the welcome view.

---
+++ LoginFlowDemo/NavigationHandler.swift
@@ -0,0 +1,26 @@
+//
+//  NavigationHandler.swift
+//  LoginFlowDemo
+//
+//  Created by Toomas Vahter on 10/04/2018.
+//  Copyright © 2018 Toomas Vahter. All rights reserved.
+//
+
+import UIKit
+
+final class NavigationHandler: NSObject, UINavigationControllerDelegate {
+
+    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        if let navigatable = viewController as? Navigatable {
+            navigationController.isNavigationBarHidden = navigatable.needsNavigationBar ? false : true
+        }
+        else {
+            navigationController.isNavigationBarHidden = true
+        }
+    }
+}
+
+protocol Navigatable {
+    var needsNavigationBar: Bool { get }
+}
+
